a726983a50ac914f29c3e8843b195cdfff68ab5e
RunLJT <68778507+Runlertjit@users.noreply.github.com>
2021-10-20 00:33:58 +0700

    Delete HW04 directory

--- HW04/071 Nitipoom/tryImport.js
+++
@@ -1,6 +0,0 @@
-import {calService, calTax, calTotal, outer, rip} from './closureassign.js'
-
-console.log(calTotal(100,calTax,calService));
-let showName = outer(); 
-showName();
-console.log(rip('poom','tus','tee','mick'));
